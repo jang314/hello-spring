abc32740f711a0bd68cb08eec87c8c40e7dff163
jang314 <jang314@naver.com>
2023-01-18 09:03:11 +0900

    [20230118] 진행중

--- src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,9 @@
 package com.hello.hellospring.repository;
 
 import com.hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
-
 
 public class MemoryMemberRepository implements MemberRepository{
 
